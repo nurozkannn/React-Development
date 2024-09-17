ee32b7d3b988bf2e4be95c48df93a0cdebf3757c
nurozkannn <ozkanhalime42@gmail.com>
2024-09-17 11:26:13 +0300

    notes added dynamically

--- keeper-app/src/Components/App.js
+++ keeper-app/src/Components/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNote(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
 }
+
 export default App;
