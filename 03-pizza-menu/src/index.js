1adede4c7a282e4f7c622c5d6b7f57fba15dd745
nurozkannn <ozkanhalime42@gmail.com>
2024-10-15 14:54:58 +0300

    Setting classes and text conditionally

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -76,12 +76,19 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu.Please come back later:)</p>
       )}
@@ -106,15 +113,15 @@ function Menu() {
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
 
-  if (pizzaObj.soldOut) return null;
+  //if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price} </span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price} </span>
       </div>
     </li>
   );
